2b024d8bbf95d90925361ae06f2c30015aed5e2c
Tim Holden <th1622ee@gmail.com>
2023-12-02 20:28:58 -0700

    changed data type to long double

--- src/physics/NIST.hpp
+++ src/physics/NIST.hpp
@@ -12,14 +12,12 @@
 #ifndef NIST_HPP
 #define NIST_HPP
 
-#include<iostream>
-
 class NIST
 {
 public:
-    const unsigned int SPEED_OF_LIGHT_IN_VACUUM = 299792458; //Units of m s^-1
-    const unsigned int NEWTONIAN_CONST_OF_GRAVITATION = 000000000066743015; //Units of m^3 kg^-1 s^-2
-    const unsigned int PLANK_CONST = 00000000000000000000000000000000062607015; //Units of J Hz^-1
+    const long double SPEED_OF_LIGHT_IN_VACUUM = 2.99792458e8; //Units of m s^-1
+    const long double NEWTONIAN_CONST_OF_GRAVITATION = 6.6743015e-11; //Units of m^3 kg^-1 s^-2
+    const long double PLANK_CONST = 6.2607015e-34; //Units of J Hz^-1
 
 };
 
